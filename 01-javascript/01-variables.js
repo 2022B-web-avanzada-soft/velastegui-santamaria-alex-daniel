570a47ffe62d756647d8408335313d8b89f52c86
Daniel Velastegui <alexdaniel.velastegui@gmail.com>
2022-11-14 17:57:46 -0500

    IMP DV variables

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,23 @@
+// Variables mutables
+var numeroUno = 1
+let numeroDos  = 2
+numeroUno = false
+numeroDos = true
+
+// Variables inmutables
+const configuracionArchivos = "PDF"
+
+// Tipos de variables primitivas
+const numero = 1 // number
+const sueldo = 1.2 // number
+const text = "Adrian" // string
+const textDos = 'Velastegui' // string (es lo mismo)
+const bool = true // boolean
+const hijos = null // object
+
+console.log(numero)
+console.log(sueldo)
+console.log(text)
+console.log(textDos)
+console.log(bool)
+console.log(hijos)
